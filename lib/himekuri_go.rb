2aaa5738bbb1ff51031cfa596f4e736114eb054f
Takayuki Kamiyama <karuma.reason@gmail.com>
2020-12-18 10:02:54 +0900

    Update, rubygems.

---
+++ lib/himekuri_go.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'open3'
+require_relative "himekuri_go/version"
+
+class HimekuriGoBasic
+  def self.before
+    # vesion info
+    ruby_version = (RUBY_VERSION).to_s
+    version = (HimekuriTs::VERSION).to_s
+    himekuri_go = "himekuri_go-".to_s + version.to_s
+    go_path= "$HOME/.goenv/shims/go".to_s
+    
+    golang_path = go_path + " " + "run" + " "+  "$HOME/.rbenv/versions/" + ruby_version + "/lib/ruby/gems/2.7.0/gems/" + himekuri_go + "/lib/himekuri_golang".to_s
+    stdout_ts, stderr_ts, status_ts = Open3.capture3(golang_path)
+    
+    stdout_ts
+    # puts stderr_ts; puts status_ts
+  end
+end
---
+++ spec/himekuri_go_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe HimekuriGo do
+  it "has a version number" do
+    expect(HimekuriGo::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
